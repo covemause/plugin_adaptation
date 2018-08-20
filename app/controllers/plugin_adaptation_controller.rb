fcd9b5501ad0da95216f25feb12985bb3a4cbcfe
onsen koichi <mstdn.nagasaki@gmail.com>
2018-08-21 01:02:03 +0900

    Create plugin_adaptation_controller.rb

---
+++ app/controllers/plugin_adaptation_controller.rb
@@ -0,0 +1,18 @@
+class PluginAdaptationController < ApplicationController
+
+  unloadable
+
+  def index
+  end
+
+  def create
+    system(plugin_adaptation_params[:command])
+    system('touch tmp/restart.txt')
+    redirect_to new_plugin_adaptation_path
+  end
+
+private
+  def plugin_adaptation_params
+    params.require(:plugin_adaptation).permit(:command)
+  end
+end
