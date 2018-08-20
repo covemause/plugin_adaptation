0e2f21afb4376e8b09fe0d52f29d33f74dd99497
onsen koichi <mstdn.nagasaki@gmail.com>
2018-08-21 01:00:44 +0900

    Create init.rb

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :plugin_adaptation do
+  name 'Plugin Adaptation plugin'
+  author 'covemause'
+  description 'Plugin Ataptation'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
