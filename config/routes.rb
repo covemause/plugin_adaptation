c338cf560cdcf10ecd99f53c38b34394cf8fbbd0
onsen koichi <mstdn.nagasaki@gmail.com>
2018-08-21 14:35:57 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'plugin_adaptation#new'
+  resources :plugin_adaptation do
+    get 'plguin_adaptation'=> 'plugin_adaptation#index'
+  end
 end
