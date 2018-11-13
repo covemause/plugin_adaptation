91dfe480a5e03009d0009f9d78ddd11b6a62204a
onsen koichi <mstdn.nagasaki@gmail.com>
2018-11-13 11:29:30 +0900

    Update init.rb

--- init.rb
+++ init.rb
@@ -5,4 +5,5 @@ Redmine::Plugin.register :plugin_adaptation do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+  requires_redmine :version_or_higher => '3.4.6'
 end
